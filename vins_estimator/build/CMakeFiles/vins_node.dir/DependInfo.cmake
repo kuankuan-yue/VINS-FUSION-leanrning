ac2e92fc3d6741ee73a7059d47777263a1723d56
kuan <liwenkuan5826@163.com>
2019-12-10 19:30:53 +0800

    first commit

---
+++ vins_estimator/build/CMakeFiles/vins_node.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kuan/1slam/VINS-Fusion/vins_estimator/src/rosNodeTest.cpp" "/home/kuan/1slam/VINS-Fusion/vins_estimator/build/CMakeFiles/vins_node.dir/src/rosNodeTest.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_PACKAGE_NAME=\"vins\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/kuan/4VINS_test/1VINS_test1/src/VINS-Fusion-master/camera_models/include"
+  "/opt/ros/kinetic/include"
+  "/opt/ros/kinetic/share/xmlrpcpp/cmake/../../../include/xmlrpcpp"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev/opencv"
+  "/usr/include/eigen3"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/kuan/1slam/VINS-Fusion/vins_estimator/build/CMakeFiles/vins_lib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
